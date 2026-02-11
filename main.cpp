d524c2073b7172466c6e8ce47f1f279c6d5808ce
thukhalwin756 <thukha.lwi@student.mahidol.edu>
2026-02-11 04:32:34 +0000

    no comment

--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-    monster A;
+    monster A("Rex", 10, 2);
+    monster B("Lulu", 7, 3);
+    monster x[5] = {{"James"},{"Pupu",22},{"Galone"},{"Malofe"},{"Bonskee"}};
+    monster c;
+    monster*p;
+    p=&c;
+    p->display();
+    p=&A;
+    p->display();
+    p=new monster("Mable",23);
+    p->display();
+    delete p;
+
+    p= new monster[3];
+    delete []p;
   /*
  
  monster *p,x;
