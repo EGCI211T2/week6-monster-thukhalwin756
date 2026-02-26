3a4625e2213918aee4d36ccb30747ce5d5077e9e
thukhalwin756 <thukha.lwi@student.mahidol.edu>
2026-02-26 11:49:52 +0000

    no comment

--- monster.h
+++ monster.h
@@ -1,3 +1,6 @@
+#include <iostream>
+#include <string>
+using namespace std;
 #ifndef monster_h
 #define monster_h
 
@@ -6,18 +9,22 @@ private:
 	string name;
 	int hp,potion;
 public:
+	monster(string= "boo", int=2,int=1);
+	~monster();
 	void Attack(monster &);
-          void heal();
-	monster(string= "boo", int=1,int=1);
-	void display(){
-	cout<<"Name: "<<name<<endl;
-	cout<<"HP: "<<hp<<endl;
-	cout<<"Potion: "<<potion<<endl;
-	}
-	~monster();	
+    void heal();
+	void display();
+	bool operator>(monster &x);
+	void setHP(int h);
+	int getHP();
 };
+	bool monster::operator>(monster &x){
+		if(hp>x.hp)return true;
+		else return false;
+
+	}
+
 	monster::~monster(){
-		cout<<name<< " is gone."<<endl;
 
 	}
 monster::monster(string na, int h, int p){
@@ -27,10 +34,18 @@ monster::monster(string na, int h, int p){
 	cout <<"Monster "<<name<<" is here"<<endl;
 
 }
-/*void display(){
+void monster::display(){
+	if(hp > 0){
 	cout<<"Name: "<<name<<endl;
 	cout<<"HP: "<<hp<<endl;
 	cout<<"Potion: "<<potion<<endl;
+	}
+};
+void monster::setHP(int h) {
+    hp = h;
+}
 
-};*/
+int monster::getHP() {
+    return hp;
+}
 #endif
