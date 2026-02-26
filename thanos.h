cc381a191bca1a4723827167b60edee4736a37f0
thukhalwin756 <thukha.lwi@student.mahidol.edu>
2026-02-26 11:50:50 +0000

    no comment

--- thanos.h
+++ thanos.h
@@ -1,15 +1,35 @@
 #ifndef thanos_h
 #define thanos_h
-
+#include"monster.h"
 class Thanos {
 private:
 	int stones;
 	int hp; 
 public:
 	/* constructor and destructor */
-	void snap_finger(monster[]); 
+	Thanos(int s=0,int h=1000); //stone hp
+	~Thanos(){
+	};
+	void snap_finger(monster[], int size); 
 	/* show all hps
-	/ clear half of monster hp, if stone =6*/
+	 clear half of monster hp, if stone =6*/
 	void operator++(); // increase the stone;
 };
+Thanos::Thanos(int s, int h){
+    stones = s;
+    hp = h;
+}
+void Thanos::snap_finger(monster m[], int size){
+	if(stones==6){
+		for (int i= 0; i<size/2; i++){
+			m[i].setHP(0);
+		}
+	}
+}
+void Thanos::operator++(){
+    if(stones < 6)
+        stones++;
+		cout<<"Number of stones: "<<stones<<endl;
+}
+
 #endif
